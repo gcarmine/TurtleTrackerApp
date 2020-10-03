c930a86ad970646042c8ca94b48335fde7bdcb21
gcarmine <gabbycarmine@gmail.com>
2020-10-03 19:48:30 -0400

    Initial commit of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Gabrielle Carmine (gabrielle.carmine@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
